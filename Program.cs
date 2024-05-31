4aa5923c17a625d47bf4979b5dcab81e413677f1
Ted Collins <theodore.collins2023@outlook.com>
2024-05-31 10:11:44 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿namespace ChallengeLabsWeek4._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = GetUserInput();
+            IsUserInputValid(s);
+        }
+
+        internal static string GetUserInput()
+        {
+            string? userInput;
+            do
+            {
+                Console.WriteLine("Enter matching open and closed brackets.");
+                userInput = Console.ReadLine();
+                return userInput;
+            } while (userInput == "");
+        }
+
+        internal static void IsUserInputValid(string input)
+        {
+            for (int i = 0; i < input.Length; i = i + 2)
+            {
+                if (input[i] != '{' && input[i] != '[' && input[i] != '(')
+                {
+                    StringValidity("Invalid");
+                    return;
+                }
+
+                if (input[i] == '{' && input[i + 1] != '{')
+                {
+                    StringValidity("Invalid");
+                    return;
+                }
+
+                if (input[i] == '[' && input[i + 1] != ']')
+                {
+                    StringValidity("Invalid");
+                    return;
+                }
+
+                if (input[i] == '(' && input[i + 1] != ')')
+                {
+                    StringValidity("Invalid");
+                    return;
+                }
+            }
+            StringValidity("Valid");
+        }
+
+        internal static void StringValidity(string valid)
+        {
+            Console.WriteLine($"{valid} String");
+        }
+    }
+}
